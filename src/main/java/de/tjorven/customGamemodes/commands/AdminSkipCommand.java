87c305857812e8c8c79743a4452c32c39d71c30c
Tjorven Grawunder <tjorven.grawunder@gmail.com>
2025-10-01 15:31:09 +0200

    fix: fixed autocompletion of adminskip command

--- src/main/java/de/tjorven/customGamemodes/commands/AdminSkipCommand.java
+++ src/main/java/de/tjorven/customGamemodes/commands/AdminSkipCommand.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collection;
+import java.util.List;
 
 public class AdminSkipCommand implements BasicCommand {
     @Override
@@ -52,6 +53,6 @@ public class AdminSkipCommand implements BasicCommand {
     @Override
     public Collection<String> suggest(CommandSourceStack commandSourceStack, String[] args) {
         String[] teamNames = TeamStorage.getInstance().getTeams().stream().map(Team::getName).toArray(String[]::new);
-        return BasicCommand.super.suggest(commandSourceStack, teamNames);
+        return List.of(teamNames);
     }
 }
